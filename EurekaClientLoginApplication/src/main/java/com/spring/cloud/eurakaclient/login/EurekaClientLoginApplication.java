75314fa987eb8d0f7a33c375ad88f9e1f1d359a7
kider <63506266+kider0506@users.noreply.github.com>
2022-03-28 12:34:34 +0530

    Initial Commit

---
+++ EurekaClientLoginApplication/src/main/java/com/spring/cloud/eurakaclient/login/EurekaClientLoginApplication.java
@@ -0,0 +1,29 @@
+package com.spring.cloud.eurakaclient.login;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaClientLoginApplication.class, args);
+	}
+
+	/**
+	 * Object creation for RestTemplate
+	 * Annotation to mark a RestTemplate bean to be configured to use a LoadBalancerClient
+	 * @return
+	 */
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
---
+++ SpringCloudGatewayServer/src/main/java/com/spring/cloud/gateway/SpringCloudGatewayServer/SpringCloudGatewayServerApplication.java
@@ -0,0 +1,15 @@
+package com.spring.cloud.gateway.SpringCloudGatewayServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class SpringCloudGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayServerApplication.class, args);
+	}
+
+}
